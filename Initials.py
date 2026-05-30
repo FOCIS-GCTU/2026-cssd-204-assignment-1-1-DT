a9a3fa80c55c9574a0331dc35b19a3e01ecb265e
1-DT <jeffreyarchar@gmail.com>
2026-05-30 23:50:27 +0000

    Add files via upload

---
+++ Initials.py
@@ -0,0 +1,33 @@
+# File: initials.py
+# Description: Print out my initials in large letters.
+# Assignment Number: 1
+#
+# Name: Jeffrey Seli Archar
+# STUDENT ID:2425401298
+# Email: jeffreyarchar@gmail.com
+# Grader: Augutus Buckman
+#
+# On my honor, Jeffrey Seli Archar, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
+def main():
+    # Print my initials in large stylized letters
+
+    print()
+    print("...JSA")
+    print()
+
+    print("...JJJJJJJJJJJJ...SSSSSSSSSSSS...AAAAAAAAAAAA...****")
+    print(".........JJJ.....SSSSSSSSSSSS...AAAA....AAAA...****")
+    print(".........JJJ.....SSS............AAAA....AAAA.......")
+    print(".........JJJ.....SSS............AAAAAAAAAAAA.......")
+    print(".........JJJ.....SSSSSSSSSSSS...AAAAAAAAAAAA.......")
+    print(".........JJJ.............SSSS...AAAA....AAAA.......")
+    print(".........JJJ.............SSSS...AAAA....AAAA.......")
+    print("JJJ......JJJ.....SSS.....SSSS...AAAA....AAAA.......")
+    print("JJJJJJJJJJJJ.....SSSSSSSSSSSS...AAAA....AAAA...****")
+    print("JJJJJJJJJJJJ.....SSSSSSSSSSSS...AAAA....AAAA...****")
+
+    print()
+main()
